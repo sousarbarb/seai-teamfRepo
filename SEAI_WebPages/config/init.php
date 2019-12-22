1311c08ca62460230a88266eb22177c909661828
Mathalin <up201505685@fe.up.pt>
2019-12-22 16:53:03 +0000

    search restrictions in database commented in order to hide errors for now

--- SEAI_WebPages/config/init.php
+++ SEAI_WebPages/config/init.php
@@ -24,6 +24,7 @@ $stmt->execute();
 
 //**************DATABASE FUNCTIONS***************
 include_once($BASE_DIR . 'database/users.php');
+// include_once($BASE_DIR . 'database/resquest.php'); - will be used when database is ready
 
 
 
