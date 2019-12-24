c1d8c1a74c2947b636093f4ce1cde374dd05519f
Daniel Baptista <up201503070@fe.up.pt>
2019-12-24 16:26:38 +0000

    Page refresh bugfix

--- SEAI_WebPages/pages/menu_my_account_change.php
+++ SEAI_WebPages/pages/menu_my_account_change.php
@@ -5,6 +5,7 @@ if(!isset($_SESSION['login']) || empty($_SESSION['login'])) {
   die(header('Location: index.php'));
 } else {
   $smarty->assign('menu', '1');
+  $_SESSION['acc_info']=$smarty->getTemplateVars('acc_info');
   $smarty->display('menu_account/menu_my_account_change.tpl');
 }
 ?>
