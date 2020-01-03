51dad2e7966dad1c523365bf51289ee74600d379
diogo <diogorodriguesmatos@gmail.com>
2020-01-03 19:32:09 +0000

    verifylandsea

--- SEAI_Final/pages/show_previous_data.php
+++ SEAI_Final/pages/show_previous_data.php
@@ -1,16 +1,23 @@
 <?php
 
 include_once('../config/init.php');
-$result=searchDataWithFilters($_SESSION["area"],null,null,null);
-//print_r($result);
 
 $smarty->assign('menu', '4');
-$smarty->assign('requests', $result);
 
 if(!isset($_SESSION['login']) || empty($_SESSION['login'])) {
   die(header('Location: index.php'));
 } else {
+$result=verifyLandOrSeaRestriction($_SESSION["area"]);
+
+if($result){
+  $result=searchDataWithFilters($_SESSION["area"],null,null,null);
+  $smarty->assign('requests', $result);
   $smarty->display('requests/Previous_data.tpl');
+}
+else{
+	$_SESSION['error_messages'][] = 'The selected area must be on the ocean';
+	die(header("Location: $BASE_URL". '/pages/map.php'));
+}
 }
 
  ?>
