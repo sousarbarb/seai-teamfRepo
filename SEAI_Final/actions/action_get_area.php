0782489354deda01c3ae49f0996242a13bde7d42
diogo <diogorodriguesmatos@gmail.com>
2020-01-05 00:06:02 +0000

    bug

--- SEAI_Final/actions/action_get_area.php
+++ SEAI_Final/actions/action_get_area.php
@@ -5,5 +5,5 @@ $req = json_decode($_POST['parea'],true);
 //print_r($req);
 $_SESSION["area"]=$req;
 print_r($_SESSION["area"]);
-header("Location: $BASE_URL". '/pages/show_previous_data.php');
+die(header('Location: ' . $BASE_URL . 'pages/show_previous_data.php'));
 ?>
